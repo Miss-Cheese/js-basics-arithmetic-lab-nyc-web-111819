11dc450951a9e6e2a375e587b222a07c8cf70eed
Miss-Cheese <nat.galligan@gmail.com>
2020-01-04 14:03:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Write your code here
-let var1 = 31;
-let var2 = 2;
-let multiply = var1 * var2;
+const var1 = 31;
+const var2 = 2;
+const multiply = var1 * var2;
 multiply;
 
-let random = Math.floor(Math.random() * 10) + 1;
+const random = Math.floor(Math.random() * 10) + 1;
 random;
 
-let mod = 16 % 6;
+const mod = 16 % 6;
 mod;
 
-let max = Math.max(2, 5, 10, 20);
+const max = Math.max(2, 5, 10, 20);
 max
